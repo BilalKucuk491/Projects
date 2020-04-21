c03ad52ac26270a11abba39a71b203efd24b731b
BilalKucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-04-21 11:05:30 +0300

    Update and rename DemetListeTipleri.py to TupleType.py

---
+++ TupleType.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 11:58:01 2020
+
+@author: BİLAL
+
+tuples =("Abel","Samuel","Richard")
+
+tuples = tuples*23 
+print(tuples)      
+
+tuples = (18,27,31,14,67,5,98,65,33,7)
+print(tuples.index(18))
+print(type(tuples))
+
+tuples = (18,27,31,14,67,5,98,65,33,7)
+tuples = tuples.index(18)
+print(tuples)
+"""
+tuples = (1,2,3,4,5,6,7,8,5,9)
+print(tuples.count(1))
