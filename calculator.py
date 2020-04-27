bc273cae46215a00f72a4ceb020eeec1bc7ddf87
BilalKucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-04-27 16:05:18 +0300

    Update and rename HesapMakinesi.py to calculator.py

---
+++ calculator.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 18:32:30 2020
+
+@author: BİLAL
+"""
+print("""
+Seçimler;
+[0] +
+[1] -
+[2] *
+[3] /
+[4] Exit
+
+Please enter the larger number in the 1st issue..
+    """)
+
+secim = float(input("You are selection:  "))
+num1 = float(input("Number one: "))
+num2 = float(input("Number two: "))
+if secim == 0 : 
+    print("\n + : {}".format(num1+num2))
+elif secim == 1:
+    print("\n - : {}".format(num1-num2))
+elif secim == 2: 
+    print("\n * : {}".format(num1*num2))
+elif secim == 3:
+    print("\n / : {}".format(num1/num2))
+elif secim == 4:
+    print("You are exit")
+else:
+    print("Hi,This is console calculator")
+        
