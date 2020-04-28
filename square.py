babc6dc10f44510831aed71ffcc49cdde3f73c51
BilalKucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-04-28 14:02:39 +0300

    Update and rename kare.py to square.py

---
+++ square.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 13:37:06 2020
+
+@author: BİLAL
+val= value
+r = diameter
+"""
+
+
+def side_square(val):
+    for i in range(int(val)):
+        print("#",end="")
+        
+
+def top_square(r):
+    for i in range(int(r/2)):
+        side_square(1)
+        
+        
+print(top_square(8))       
+            
