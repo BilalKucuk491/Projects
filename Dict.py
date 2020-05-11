1371c1d3421e17bd01615c26cf2c637eac1505cf
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-11 22:54:10 +0300

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 22:51:12 2020
+
+@author: BİLAL
+"""
+
+#does not enter the same values True = 1 and False = 0 
+# try it 
+dict = {"Bilal",1,True,False,0}
+print(dict)
