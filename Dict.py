db4b2776ce22fe6e895455e80e1dd9427f1faf22
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-12 19:06:01 +0300

    Update Dict.py

--- Dict.py
+++ Dict.py
@@ -9,3 +9,24 @@ Created on Mon May 11 22:51:12 2020
 # try it and see
 dict = {"Bilal",1,True,False,0}
 print(dict)
+
+vegatable = input("Please enter a vegatables")
+class Vegan():
+  vegaList = []
+  def __init__ ():
+    print("Please enter veatables value")
+  def vegatables(self,bieber,lime,cucumber,onion):
+    self.bieber = Bieber
+    self.lime = Lime
+    self.cucumber = Cucumber
+    self.onion = Onion
+    vegalist.append[self.bieber]
+    vegalist.append[self.lime]
+    vegalist.append[self.cucumber]
+    vegalist.append[self.onion]
+  
+  def ShowOnBasket():
+    print(vegaList)
+    
+Vegan = basket(12,4,2,5)
+print(Vegan.ShowOnBasket)
