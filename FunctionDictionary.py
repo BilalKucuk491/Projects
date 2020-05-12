ace5a552a3511e655dadcb8426fa9cdb562e5040
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-12 19:46:34 +0300

    Update and rename Dict.py to FunctionDictionary.py

---
+++ FunctionDictionary.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 22:51:12 2020
+
+@author: BİLAL
+"""
+
+#does not enter the same values True = 1 and False = 0 
+# try it and see
+dict = {"Bilal",1,True,False,0}
+print(dict)
+
+class dictionary:
+  def __init__(self,plants,animals):
+    self.colors = colors
+    self.animals = animals
+  def AddDict(self):
+    list  = []
+    list[0] = self.colors
+    list[1] = self.colors
+    return list
+  def ShowOn(self):
+    print(list)
+    
+dic = dictionary("Blue","Reptile")
+dic.ShowOn()
+    
