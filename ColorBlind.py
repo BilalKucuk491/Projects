e09a552ec649fc1f2e6cc7564678306a6d81f5dc
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-12 17:51:48 +0300

    Add files via upload

---
+++ ColorBlind.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 19:57:50 2020
+
+@author: BİLAL
+Color-Blind-"T"este
+
+ColorBlind 1.0
+
+"""
+x = int(input("Female : "))
+y = int(input("Male : "))
+
+def colorBlind(x,y):
+    #rr = 0
+    #Rr = 1
+    #RR = 2
+    print("""
+          0 = Diseased
+          1 = Disease Carrier
+          2 = Healthy
+          """)
+    if x == 0 and y == 2:
+        print("\n%50 Diseased %50 Disease Carrier")
+    elif x == 1 and y == 2:
+        print("\n%25 Diseased %25 Disease Carrier %50 Healthy  ")
+    elif x == 1 and y == 0:
+        print("\n%25 Diseased %25 Disease Carrier %50 Healthy  ")    
+    elif x == 2 and y == 0:
+        print("\n%50 Disease Carrier %50 Healthy")  
+    elif x == 2 and y == 2:
+        print("\n%100 Healthy")
+    elif x == 0 and y == 0:
+        print("\n%100 Diseased")
+    else:
+        print("Error")
+    return x,y    
+
+print(colorBlind(x, y))        
+
+
