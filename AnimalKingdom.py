623919fefc3665b1180bdacfb7d49a27c807e43a
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-16 22:58:02 +0300

    Add files via upload

---
+++ AnimalKingdom.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 22:14:11 2020
+
+@author: BİLAL
+"""
+
+class invertebrates(): #Omurgasızlar
+    
+    def sponges(): #Süngerler
+        pass 
+    def solent(): #Sölenterler
+        pass 
+    def worms(): #Solucanlar
+        
+        def flatworms(): #Yassı Solucanlar
+            pass  
+        def round_Worms(): #Yuvarlak Solucanlar
+            pass 
+        def ringed_Worms(): #Halkalı Solucanlar
+            pass 
+        
+        
+    def mollusc(): #Yumuşakçalılar
+        pass 
+    
+    def arthropod(): #Eklem Bacaklılar
+        
+         def crustaceans(): #Kabuklular
+             pass 
+         def arachnid(): #Örümceğisiler
+             pass 
+         def multi_Legged(): #Çok ayaklılar
+             pass
+         def insects(): #Böcekler
+            pass 
+        
+    def echinoderm(): #Derisi Dikenliler
+        pass 
+
+
+class vertebrates(): #Omurgalılar
+    
+    def fishes(self): #Balıklar
+        pass
+    def two_Living(self): #İki Yaşamlılar
+        pass
+    def birds(self): #Kuşlar
+        pass 
+    def mammals(self): #Memeliler
+        
+        def beaked_Mammals(self): #Gagalı Memeliler
+            pass 
+        def marsupial_Mammals(self): #Keseli Memeliler
+            pass 
+        def plecantel_Mammals(self): #Plesantalı Memeliler
+            pass 
