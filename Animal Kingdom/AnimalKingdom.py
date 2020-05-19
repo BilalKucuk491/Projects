027a49cdea92e7fb8764c83d7c1e563d96ae4414
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-19 19:38:44 +0300

    Add files via upload

--- Animal Kingdom/AnimalKingdom.py
+++ Animal Kingdom/AnimalKingdom.py
@@ -5,50 +5,56 @@ Created on Sat May 16 22:14:11 2020
 @author: BİLAL
 """
 
-class invertebrates(): 
+class invertebrates(): #Omurgasızlar
     
-    def sponges(): 
+    def sponges(): #Süngerler
        with open("sponges.txt","r") as sponge:
                print(sponge.read())
              
-    def solent(): 
+    def solent(): #Sölenterler
          with open("solen.txt","r") as solen:
              print(solen.read())
              
-    def worms():
+    def worms(): #Solucanlar
         with open("worm.txt","r") as worm:
             print(worm.read())
        
-    def molluscs(): 
+    def molluscs(): #Yumuşakçalılar
         with open("mollusc.txt","r") as mollusc:
             print(mollusc.read())
     
-    def arthropod(): 
+    def arthropod(): #Eklem Bacaklılar
         
         with open("arth.txt","r") as arth:
             print(arth.read())
         
-    def echinoderm(): 
+    def echinoderm(): #Derisi Dikenliler
         with open("echino.txt","r") as echino:
             print(echino.read())
 
 
-class vertebrates(): 
+class vertebrates(): #Omurgalılar
     
-    def fishes(self): 
-        pass
-    def two_Living(self): 
-        pass
-    def birds(self):
-        pass 
-    def mammals(self): 
+    def fishes(): #Balıklar
+        with open("fish.txt","r") as fish:
+            print(fish.read())
+            
+    def two_Living(): #İki Yaşamlılar
+        with open("frog.txt","r") as frog:
+            print(frog.read())
+            
+    def reptiles(): #Kuşlar
+        with open("reptile.txt","r") as reptile:
+            print(reptile.read())  
+            
+    def birds(): #Kuşlar
+        with open("bird.txt","r") as bird:
+            print(bird.read())
+            
+    def mammals(): #Memeliler
         
-        def beaked_Mammals(self): 
-            pass 
-        def marsupial_Mammals(self): 
-            pass 
-        def plecantel_Mammals(self): 
-            pass 
+        with open("mammal.txt","r") as mammal:
+            print(mammal.read())
         
         
-invertebrates.echinoderm()
+vertebrates.mammals()
