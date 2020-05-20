7a616b95ad18da7889089276e5eac17eec3fb0c9
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-20 16:46:48 +0300

    Update square.py

--- square.py
+++ square.py
@@ -9,9 +9,12 @@ r = diameter
 
 class Square:
     def __init__(self):
+        num = int(input(""))
+        r = int(input("Please enter a diameter."))
+     return num
         
-    def side_square(val):
-        for i in range(int(val)):  
+    def side_square(num):
+        for i in range(int(num)):  
          print("#",end="")
     
     def top_square(r):
