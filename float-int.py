c995475c7569f4aaae869c4ac96f249f49c80130
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-24 00:06:08 +0300

    Update float-int.py

--- float-int.py
+++ float-int.py
@@ -4,11 +4,10 @@ Created on Thu Apr  9 12:50:58 2020
 
 @author: BİLAL
 
-daire = circle
-silindir çapı = cylinder diameter
 """
 
 """
+******************************************
 a=10
 b=2
 print(a**2)
@@ -21,19 +20,19 @@ print( 2*a)
 print('C+'*12)
 print(" :) "*10)
 
-
+*******************************************
 full_name = "John Smith"
 patient_age = 20
 new_patient = True
 print(full_name,patient_age)
-
+***************************************
 name = input(" what is your name? ")
 print ("Hi"+name)
-
+**************************************
 a=4
 b=6
 print(a*b)
-
+**************************************
 birth_year = input("Birth year :")
 age = 2020-int(birth_year)
 print(age)
@@ -41,27 +40,28 @@ print(age)
 kisi_kilosu = input("Kilonuz: ")
 kilogram = "Kilonuz: "+kisi_kilosu+" kg"
 print(kilogram)
-
+*************************************
 Dairenin alanı = 2.pi.r
 daire_capi = input("Daire Çapı: ")
 pi = 3.14
 alani = int(daire_capi) *2*pi
 print(alani)
-
+*************************************
 Silindir alanı = 2.pi.r(r+h)
 
-silindir_capi = input("Silindirin çapı: ")
-silindir_yuksekligi = input("Silindir yüksekliği: ")
+cylinder_di = input("Diameter of cylinder: ")
+cylinder_height = input("Height of cylinder: ")
 pi = 3.14
-silindir_alani = 2*pi*int(silindir_capi)*(int(silindir_yuksekligi) + int(silindir_capi))
+cylinder_field = 2*pi*int(cylinder_di)*(int(cylinder_height) + int(cylinder_di))
 print(silindir_alani) 
-
-u_kenar=input("uzun kenar: ")
-o_kenar=input("orta kenar: ")
-k_kenar=input("kısa kenar: ")
-dik_alani = int(u_kenar)* int(k_kenar)*int(o_kenar)
-print(dik_alani)
+*************************************
+l_edge=input("long edge: ")
+m_edge=input("middle edge: ")
+s_edge=input("short edge: ")
+rectangular_field = int(l_edge)* int(s_edge)*int(m_edge)
+print(rectangular_field)
+************************************
 """
-sayi = 5 
-print(sayi)
+num = 5 
+print(num)
 
