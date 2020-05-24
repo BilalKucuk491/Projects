de7bd61312af723e1b2489e4c9eedaf28168a9dc
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-24 18:19:22 +0300

    Update AnimalKingdom.py

--- Animal Kingdom/AnimalKingdom.py
+++ Animal Kingdom/AnimalKingdom.py
@@ -2,12 +2,13 @@
 """
 Created on Sat May 16 22:14:11 2020
 @author: BİLAL
-Version 1.2
+Version 1.2.1
+
+
 """
 from interface import interFace
-  
-
 interFace()
+
     
         
                
