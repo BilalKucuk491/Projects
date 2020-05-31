9714bed7269c4846fe14daf6e642f6b3e6e9ce76
Bilal Kucuk <64019703+BilalKucuk491@users.noreply.github.com>
2020-05-31 15:34:11 +0300

    Add files via upload

---
+++ Drinks/coffee.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 13:49:06 2020
+
+@author: BİLAL
+"""
+
+from sqlCommant import sqlCo
+import time
+
+def interface():
+    print("""
+                  
+            1)Create table
+            2)Add table
+            3)Show on table
+            4)Update Table
+            5)Delete column
+            6)EXİT
+                  """)  
+
+def dodnet():
+    for i in range(3): 
+                print(".",end="")
+                time.sleep(1)            
+while 1:
+    try:
+        menu = input("You are selection exit : 0 next : 1 ")
+        if menu == "1":
+            dodnet()
+                
+            interface()
+            selection = int(input("You are selection: "))
+            
+            if selection ==1:
+                sqlCo.create_tble()
+                
+            elif selection == 2:
+                d_name = input("Drink Name: ")
+                price = int(input("Drink Price: "))
+                piece = int(input("Drink Piece: "))
+                sqlCo.add_tble(d_name, piece, price)
+                
+            elif selection == 3:
+                sqlCo.showOn_tble()
+            elif selection == 4:
+                
+                old_price = int(input("Old Drink price: "))
+                new_price = int(input("New Drink price: "))
+                sqlCo.update_tble(old_price, new_price)
+                
+            elif selection == 5:
+                d_name = input("Drink Name: ")
+                sqlCo.delete_column_tble(d_name)
+                dodnet()
+                print("Delte column = {}".format(d_name))            
+        else:
+            break
+    except ValueError:
+        print("Value Type Error")    
+   
---
+++ Drinks/sqlCommant.py
@@ -0,0 +1,27 @@
+import sqlite3 
+baglanti = sqlite3.connect("market.db")
+d_cursor = baglanti.cursor()
+
+class sqlCo():
+    def create_tble():
+        d_cursor.execute("create table if not exists drinks (d_name TEXT,piece INT,price INT)")
+        baglanti.commit()
+    def add_tble(d_name,piece,price):
+        d_cursor.execute("insert into drinks values(?,?,?)",(d_name,piece,price))
+        baglanti.commit()
+    
+    def showOn_tble():
+        d_cursor.execute("select* from drinks")
+        table_datas =d_cursor.fetchall()
+        for table_data in table_datas:
+            print(table_data)
+    def update_tble(old_price,new_price):
+        d_cursor.execute("update drinks set price = ? where price = ? ",(new_price,old_price))
+        baglanti.commit()
+    def delete_column_tble(d_name):
+        d_cursor.execute("delete from Drinks where d_name= ? ",(d_name,))
+"""        
+create_tble()
+update_tble(18, 21)
+showOn_tble()
+"""
